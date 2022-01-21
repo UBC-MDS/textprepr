753aa99e70d1a4701e254c521a4ccffa407ab4de
Melisa Maidana <88546099+mmaidana24318@users.noreply.github.com>
2022-01-21 09:53:50 -0800

    add def extract_hashtags

---
+++ R/extract_hashtags.R
@@ -0,0 +1,17 @@
+#' Extracts all hashtags included in the input tweet data list
+#'
+#' @param tweets A vector of tweets
+#'
+#' @return Vector of hashtags
+#' @export
+#'
+#' @examples
+#' tweets <- c(
+#'    "Make America Great Again #DonaldTrump"
+#' )
+#' extract_hashtags(tweets)
+#'>[1] "DonaldTrump"
+extract_hashtags <- function(tweets) {
+
+}
+
