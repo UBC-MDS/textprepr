475d1995520eab0b4e5c7901486ef4d8a5926921
Arijeet Chatterjee <arijeet.chatterjee@gmail.com>
2022-01-24 22:11:25 -0800

    feat: add code for function extract_ngram and associated tests

---
+++ tests/testthat/test-extract_ngram.R
@@ -0,0 +1,22 @@
+# Test for function extract_ngram
+helper_data <- c("Make", "America", "Great", "Again", "DonaldTrump")
+test_that("Generated ngrams is incorrect", {
+  expect_equal(extract_ngram(helper_data, n=1), c("Make", "America", "Great", "Again", "DonaldTrump"))
+})
+test_that("Generated ngrams is incorrect", {
+  expect_equal(extract_ngram(helper_data, n=3), c("Make America Great", "America Great Again", "Great Again DonaldTrump"))
+})
+test_that("Generated ngrams is incorrect", {
+  expect_equal(extract_ngram(helper_data, n=4), c("Make America Great Again", "America Great Again DonaldTrump"))
+})
+test_that("Generated ngrams is incorrect", {
+  expect_equal(extract_ngram(helper_data, n=5), c("Make America Great Again DonaldTrump"))
+})
+
+test_that("Length of ngrams should be less than length of tweets", {
+  expect_error(extract_ngram(helper_data, n=10))
+})
+
+test_that("Input should be character vector", {
+  expect_error(extract_ngram(data.frame(), n=10))
+})
