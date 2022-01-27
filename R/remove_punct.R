d602a3340b4e0c48f246c9e289a0ff6b44ba50a0
philschan <philsonchan@gmail.com>
2022-01-26 17:45:23 -0800

    Complete remove_punct implementation

--- R/remove_punct.R
+++ R/remove_punct.R
@@ -18,5 +18,27 @@
 #' remove_punct(tweets)
 #' remove_punct(tweets, skip = c("'", "@", "#", "-"))
 remove_punct <- function(tweets, skip = NULL) {
+  if (is.null(tweets))
+    return (NULL)
 
+  if (is.null(skip)){
+    match_pattern <- "[^A-Za-z\\s\\d]"
+  } else if (is.vector(skip) && is.character(skip)){
+    skip_chars = paste(skip, collapse = "")
+    match_pattern <- paste("[^A-Za-z\\s\\d", skip_chars, "]", sep="")
+  } else {
+    stop("Please pass vector of characters to `skip` parameter")
+  }
+
+  if (is.vector(tweets) && is.character(tweets)){
+    stringr::str_replace_all(tweets, match_pattern, "")
+  } else if (is.list(tweets)) {
+    # Throw error if not all list elements are character or strings
+    if (!purrr::map_lgl(tweets, ~class(.) == "character") |> all()) {
+      stop("Only list with all string elements can be passed")
+    }
+    purrr::map(tweets, ~stringr::str_replace_all(., match_pattern, ""))
+  } else {
+    stop("Please pass list/vector of strings as the input")
+  }
 }
