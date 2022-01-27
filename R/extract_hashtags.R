d570939f4b615bb532ed686f6ce0dd9a337cc689
Melisa Maidana <88546099+mmaidana24318@users.noreply.github.com>
2022-01-26 17:07:14 -0800

    delete incorrect exception handler

--- R/extract_hashtags.R
+++ R/extract_hashtags.R
@@ -15,9 +15,6 @@ extract_hashtags <- function(tweets) {
   if(!is.character(tweets)) {
     stop("input should be a character vector")
   }
-  if(length(tweets)==0) {
-    stop("input vector cannot be null")
-  }
   output_vec <- stringr::str_extract_all(tweets, "#\\S+")
   output_vec <-   stringr::str_remove_all(unlist(output_vec), "#")
   output_vec
